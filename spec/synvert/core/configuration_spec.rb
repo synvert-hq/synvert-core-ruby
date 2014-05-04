a4158be0a6379c4368e8ef6c31c6e96d380b21aa
Richard Huang <flyerhzm@gmail.com>
2014-05-04 14:23:14 +0800

    test configuration

---
+++ spec/synvert/core/configuration_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+module Synvert::Core
+  describe Configuration do
+    it 'sets / gets' do
+      Configuration.instance.set :key, 'value'
+      expect(Configuration.instance.get :key).to eq 'value'
+    end
+  end
+end
