00e61393822d09558bd3711587d1e4785a332158
Richard Huang <flyerhzm@gmail.com>
2014-08-11 23:12:48 +0800

    use mocha as mock framework

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,8 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
+  config.mock_framework = :mocha
+
   config.order = 'random'
 
   config.before do
