d8e176f0e00f429c43f67a4e1a2126c71cf788d5
Richard Huang <flyerhzm@gmail.com>
2014-09-05 23:22:09 +0800

    leave dsl code together

--- lib/synvert/core/rewriter/instance.rb
+++ lib/synvert/core/rewriter/instance.rb
@@ -119,6 +119,27 @@ module Synvert::Core
       @current_node
     end
 
+    # Set current_node to node and process.
+    #
+    # @param node [Parser::AST::Node] node set to current_node
+    # @yield process
+    def process_with_node(node)
+      self.current_node = node
+      yield
+      self.current_node = node
+    end
+
+    # Set current_node properly, process and set current_node back to original current_node.
+    #
+    # @param node [Parser::AST::Node] node set to current_node
+    # @yield process
+    def process_with_other_node(node)
+      original_node = self.current_node
+      self.current_node = node
+      yield
+      self.current_node = original_node
+    end
+
     #######
     # DSL #
     #######
@@ -219,27 +240,6 @@ module Synvert::Core
     # @param message [String] warning message.
     def warn(message)
       @rewriter.add_warning Rewriter::Warning.new(self, message)
-    end
-
-    # Set current_node to node and process.
-    #
-    # @param node [Parser::AST::Node] node set to current_node
-    # @yield process
-    def process_with_node(node)
-      self.current_node = node
-      yield
-      self.current_node = node
-    end
-
-    # Set current_node properly, process and set current_node back to original current_node.
-    #
-    # @param node [Parser::AST::Node] node set to current_node
-    # @yield process
-    def process_with_other_node(node)
-      original_node = self.current_node
-      self.current_node = node
-      yield
-      self.current_node = original_node
     end
 
   private
