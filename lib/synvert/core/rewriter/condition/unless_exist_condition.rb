ffa09e7cc4680b4d258fe80590c5b52d31e9ee23
Awesome Code <team@xinminlabs.com>
2021-01-09 07:17:03 +0000

    Auto corrected by following Lint Ruby Style/SafeNavigation

--- lib/synvert/core/rewriter/condition/unless_exist_condition.rb
+++ lib/synvert/core/rewriter/condition/unless_exist_condition.rb
@@ -8,7 +8,7 @@ module Synvert::Core
     def match?
       match = false
       @instance.current_node.recursive_children do |child_node|
-        match = match || (child_node && child_node.match?(@rules))
+        match = match || (child_node&.match?(@rules))
       end
       !match
     end
