11fb486417d233259fe6b37379afa80b859acfff
Awesome Code <team@xinminlabs.com>
2021-01-09 07:05:56 +0000

    Auto corrected by following Lint Ruby Parentheses

--- spec/synvert/core/configuration_spec.rb
+++ spec/synvert/core/configuration_spec.rb
@@ -4,7 +4,7 @@ module Synvert::Core
   describe Configuration do
     it 'sets / gets' do
       Configuration.instance.set :key, 'value'
-      expect(Configuration.instance.get :key).to eq 'value'
+      expect(Configuration.instance.get(:key)).to eq 'value'
     end
   end
 end
