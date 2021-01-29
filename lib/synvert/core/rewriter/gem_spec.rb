4c9aa8f954de72fc5989e67c702eaf8db5bc716d
Richard Huang <flyerhzm@gmail.com>
2021-01-29 10:31:16 +0800

    ignore gem_spec check if Gemfile.lock does not exist

--- lib/synvert/core/rewriter/gem_spec.rb
+++ lib/synvert/core/rewriter/gem_spec.rb
@@ -27,15 +27,14 @@ module Synvert::Core
     # @raise [Synvert::Core::GemfileLockNotFound] raise if Gemfile.lock does not exist.
     def match?
       gemfile_lock_path = File.join(Configuration.instance.get(:path), 'Gemfile.lock')
-      if File.exists? gemfile_lock_path
-        parser = Bundler::LockfileParser.new(File.read(gemfile_lock_path))
-        if spec = parser.specs.find { |spec| spec.name == @name }
-          Gem::Version.new(spec.version).send(OPERATORS[@operator], @version)
-        else
-          false
-        end
+      # if Gemfile.lock does not exist, just ignore this check
+      return true unless File.exist?(gemfile_lock_path)
+
+      parser = Bundler::LockfileParser.new(File.read(gemfile_lock_path))
+      if spec = parser.specs.find { |spec| spec.name == @name }
+        Gem::Version.new(spec.version).send(OPERATORS[@operator], @version)
       else
-        raise GemfileLockNotFound.new 'Gemfile.lock does not exist'
+        false
       end
     end
   end
--- spec/synvert/core/rewriter/gem_spec_spec.rb
+++ spec/synvert/core/rewriter/gem_spec_spec.rb
@@ -17,37 +17,37 @@ GEM
     "}
 
     it 'returns true if version in Gemfile.lock is greater than definition' do
-      expect(File).to receive(:exists?).with('./Gemfile.lock').and_return(true)
+      expect(File).to receive(:exist?).with('./Gemfile.lock').and_return(true)
       expect(File).to receive(:read).with('./Gemfile.lock').and_return(gemfile_lock_content)
       gem_spec = Rewriter::GemSpec.new('ast', {gte: '1.0.0'})
       expect(gem_spec).to be_match
     end
 
     it 'returns true if version in Gemfile.lock is equal to definition' do
-      expect(File).to receive(:exists?).with('./Gemfile.lock').and_return(true)
+      expect(File).to receive(:exist?).with('./Gemfile.lock').and_return(true)
       expect(File).to receive(:read).with('./Gemfile.lock').and_return(gemfile_lock_content)
       gem_spec = Rewriter::GemSpec.new('ast', '1.1.0')
       expect(gem_spec).to be_match
     end
 
     it 'returns false if version in Gemfile.lock is less than definition' do
-      expect(File).to receive(:exists?).with('./Gemfile.lock').and_return(true)
+      expect(File).to receive(:exist?).with('./Gemfile.lock').and_return(true)
       expect(File).to receive(:read).with('./Gemfile.lock').and_return(gemfile_lock_content)
       gem_spec = Rewriter::GemSpec.new('ast', {gt: '1.2.0'})
       expect(gem_spec).not_to be_match
     end
 
     it 'returns false if gem does not exist in Gemfile.lock' do
-      expect(File).to receive(:exists?).with('./Gemfile.lock').and_return(true)
+      expect(File).to receive(:exist?).with('./Gemfile.lock').and_return(true)
       expect(File).to receive(:read).with('./Gemfile.lock').and_return(gemfile_lock_content)
       gem_spec = Rewriter::GemSpec.new('synvert', '1.0.0')
       expect(gem_spec).not_to be_match
     end
 
     it 'raise Synvert::Core::GemfileLockNotFound if Gemfile.lock does not exist' do
-      expect(File).to receive(:exists?).with('./Gemfile.lock').and_return(false)
+      expect(File).to receive(:exist?).with('./Gemfile.lock').and_return(false)
       gem_spec = Rewriter::GemSpec.new('ast', '1.1.0')
-      expect { gem_spec.match? }.to raise_error(Synvert::Core::GemfileLockNotFound)
+      expect(gem_spec).to be_match
     end
   end
 end
