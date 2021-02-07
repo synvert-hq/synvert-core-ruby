500196ecc12666a540f4ac3a743ae2c27e1ab8d9
Awesome Code <team@xinminlabs.com>
2021-02-07 05:46:40 +0000

    Auto corrected by following Lint Ruby Style/ParallelAssignment

--- lib/synvert/core/rewriter.rb
+++ lib/synvert/core/rewriter.rb
@@ -56,7 +56,8 @@ module Synvert::Core
       # @param name [String] the unique rewriter name.
       # @param rewriter [Synvert::Core::Rewriter] the rewriter to register.
       def register(group, name, rewriter)
-        group, name = group.to_s, name.to_s
+        group = group.to_s
+        name = name.to_s
         rewriters[group] ||= {}
         rewriters[group][name] = rewriter
       end
@@ -68,7 +69,8 @@ module Synvert::Core
       # @return [Synvert::Core::Rewriter] the matching rewriter.
       # @raise [Synvert::Core::RewriterNotFound] if the registered rewriter is not found.
       def fetch(group, name)
-        group, name = group.to_s, name.to_s
+        group = group.to_s
+        name = name.to_s
         if exist? group, name
           rewriters[group][name]
         else
@@ -83,7 +85,8 @@ module Synvert::Core
       # @return [Synvert::Core::Rewriter] the registered rewriter.
       # @raise [Synvert::Core::RewriterNotFound] if the registered rewriter is not found.
       def call(group, name)
-        group, name = group.to_s, name.to_s
+        group = group.to_s
+        name = name.to_s
         if exist? group, name
           rewriter = rewriters[group][name]
           rewriter.process
@@ -99,7 +102,8 @@ module Synvert::Core
       # @param name [String] the rewriter name.
       # @return [Boolean] true if the rewriter exist.
       def exist?(group, name)
-        group, name = group.to_s, name.to_s
+        group = group.to_s
+        name = name.to_s
         if rewriters[group] && rewriters[group][name]
           true
         else
