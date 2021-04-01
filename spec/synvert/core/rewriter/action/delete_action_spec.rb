2307f51558ccd40ac3b3a2b539697091f4241ed0
Awesome Code <team@xinminlabs.com>
2021-04-01 13:57:00 +0000

    Auto corrected by following Format Ruby Code

--- spec/synvert/core/rewriter/action/delete_action_spec.rb
+++ spec/synvert/core/rewriter/action/delete_action_spec.rb
@@ -5,18 +5,18 @@ require 'spec_helper'
 module Synvert::Core
   describe Rewriter::DeleteAction do
     subject {
-      source = "arr.map {}.flatten"
+      source = 'arr.map {}.flatten'
       node = Parser::CurrentRuby.parse(source)
       instance = double(current_node: node)
       Rewriter::DeleteAction.new(instance, :dot, :message)
     }
 
     it 'gets begin_pos' do
-      expect(subject.begin_pos).to eq "arr.map {}".length
+      expect(subject.begin_pos).to eq 'arr.map {}'.length
     end
 
     it 'gets end_pos' do
-      expect(subject.end_pos).to eq "arr.map {}.flatten".length
+      expect(subject.end_pos).to eq 'arr.map {}.flatten'.length
     end
 
     it 'gets rewritten_code' do
