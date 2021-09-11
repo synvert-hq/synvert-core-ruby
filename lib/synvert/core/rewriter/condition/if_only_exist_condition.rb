1596a25a703c7141e45d18c8c009562d9a133fe5
Richard Huang <flyerhzm@gmail.com>
2021-09-11 08:33:59 +0800

    fix comment

--- lib/synvert/core/rewriter/condition/if_only_exist_condition.rb
+++ lib/synvert/core/rewriter/condition/if_only_exist_condition.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Synvert::Core
-  # IfExistCondition checks if node has only one child node and the child node matches rules.
+  # IfOnlyExistCondition checks if node has only one child node and the child node matches rules.
   class Rewriter::IfOnlyExistCondition < Rewriter::Condition
     # check if only have one child node and the child node matches rules.
     def match?
