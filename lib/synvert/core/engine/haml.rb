6b43973135783f291bda6b21d4f8e37288096ed4
Awesome Code <team@xinminlabs.com>
2023-04-05 05:17:23 +0000

    Auto corrected by following Format Ruby Code

--- lib/synvert/core/engine/haml.rb
+++ lib/synvert/core/engine/haml.rb
@@ -13,23 +13,26 @@ module Synvert::Core
         # @return [String] encoded ruby code.
         def encode(source)
           tab_sizes = []
-          lines = source.lines.map do |line|
-            if line =~ /\A(\s*)(- ?)(.*)/ # match "  - if currenet_user"
-              code = ' ' * ($1.size + $2.size) + $3
-              new_line = $3.start_with?('else', 'elsif', 'when') ? code : check_and_insert_end(code, tab_sizes, $1.size)
-              tab_sizes.push($1.size) if $3.start_with?('if', 'unless', 'begin', 'case') || $3.include?(' do ')
-              new_line
-            else
-              if line =~ /\A(\s*)([%#\.].*)?=(.*)/ # match "  %span= current_user.login"
-                code = ' ' * ($1.size + ($2 || '').size + 1) + $3
-                new_line = check_and_insert_end(code, tab_sizes, $1.size)
-                tab_sizes.push($1.size) if line.include?(' do ')
+          lines =
+            source.lines.map do |line|
+              if line =~ /\A(\s*)(- ?)(.*)/ # match "  - if currenet_user"
+                code = ' ' * ($1.size + $2.size) + $3
+                new_line =
+                  $3.start_with?('else', 'elsif', 'when') ? code : check_and_insert_end(code, tab_sizes, $1.size)
+                tab_sizes.push($1.size) if $3.start_with?('if', 'unless', 'begin', 'case') || $3.include?(' do ')
+
                 new_line
-              elsif line =~ /\A(\s*)(.*)/ # match any other line
-                check_and_insert_end(' ' * line.size, tab_sizes, $1.size)
+              else
+                if line =~ /\A(\s*)([%#\.].*)?=(.*)/ # match "  %span= current_user.login"
+                  code = ' ' * ($1.size + ($2 || '').size + 1) + $3
+                  new_line = check_and_insert_end(code, tab_sizes, $1.size)
+                  tab_sizes.push($1.size) if line.include?(' do ')
+                  new_line
+                elsif line =~ /\A(\s*)(.*)/ # match any other line
+                  check_and_insert_end(' ' * line.size, tab_sizes, $1.size)
+                end
               end
             end
-          end
           lines.push(END_LINE) unless tab_sizes.empty?
           lines.join("\n")
         end
