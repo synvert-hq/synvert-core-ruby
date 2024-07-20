e57ec7d5700fff7f5b4840921b7add77f7cd77a3
Richard Huang <flyerhzm@gmail.com>
2024-07-20 19:08:08 +0800

    remove unused variable

--- spec/synvert/core/rewriter_spec.rb
+++ spec/synvert/core/rewriter_spec.rb
@@ -346,11 +346,10 @@ module Synvert::Core
       it 'eval helper by name' do
         block_receiver = nil
         block_options = {}
-        helper =
-          Synvert::Helper.new 'helper' do |options|
-            block_receiver = self.class.name
-            block_options = options
-          end
+        Synvert::Helper.new 'helper' do |options|
+          block_receiver = self.class.name
+          block_options = options
+        end
         rewriter =
           Rewriter.new 'group', 'rewriter' do
             call_helper('helper', foo: 'bar')
